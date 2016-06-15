3b15c2845fcac62117c484fea5c578155a65bc0e
Julian Heavyside <julianheavyside@gmail.com>
2016-06-15 12:12:54 -0700

    fixed it all

--- sampling-effects.R
+++ sampling-effects.R
@@ -1,217 +1,130 @@
 rm(list=ls())
 library(diversitree)
-library(ape)
-library(geiger)
 
 ### Effects of sampling (missing data) on parameters of models of discrete character evolution
 # this focuses on continuous-time Markov models of discrete binary character evolution, which hold diversification rates constant and allow the character state of a lineage to transition from state to state
+# can be extended to BiSSE model
 # we are specifically interested in producing MLEs for the following two parameters: q01 = transition rate from state 0 to state 1, and q10 = transition rate from state 1 to state 0
 
-# start by simulating a tree with 2000 tips, lambda = 0.2, and mu = 0.03
+
+# how sensitive are parameters (q01 and q10) to varying amounts of missing data
+# ability of model comparison to reject a null hypothesis
+# adjust tree size, relative rates of q01 and q10, lambda and mu, etc
+
+
+# set number of tree tips (tree size)
+max.taxa <- 2000
+
+# start by simulating a tree of size "max.taxa"
 set.seed(2)
-tree <- tree.bd(c(.2, .03), max.taxa=2000)
+tree <- tree.bd(pars=c(.2, .03), max.taxa=max.taxa) # pars=c(lambda, mu)
 
 # simulate a binary trait exhibiting a rate of evolution from state 0 to state 1 within a lineage of 0.1, and 0.2 for state 1 to state 0. Use a Mk2 (Markov binary character) model of character evolution
 set.seed(1)
-states <- sim.character(tree, c(.1, .2), model="mk2")
-length(which(states==1))
+states <- sim.character(tree, pars=c(.1, .2), model="mk2") # pars=c(q01, q10)
+
+# check how many tips are in state 0 (should be about half if q01=q10)
+length(which(states==0))
 
 # build a likelihood function based on Mk2 model (used to find likelihood of parameter values: probability of the observed distribution of character states given that the tree evolved according to a given model of character evolution and/or diversification)
-lik.mk2 <- make.mk2(tree, states)
+lik <- make.mk2(tree, states)
 
 # run ML analysis using an initial parameter guess of (0.1, 0.1)
-# I tried with initial guesses ranging from (0.1, 0.9) to (0.9, 0.1) and similar MLEs are produced in each case
-fit.mk2 <- find.mle(lik.mk2, c(.1, .1), method="subplex")
-
-# remember, the tree is simulated, so we know the true values of the parameters q01=0.1 and q10=0.2
+fit <- find.mle(lik, x.init=c(.1, .1))
 
 # parameter estimates for q01 and q10 from the fit
-coef(fit.mk2)
-# q01        q10 
-# 0.08399429 0.23020744 
-# difference is slightly larger than the true model parameters (0.1 and 0.2)
-
-# compare our Markov model fit to a null hypothesis model (with parameters q01 = q10) to test if the Mk fit is statistically justified
-
-# build likelihood function based on null model of q01 = q10
-lik.mk1 <- constrain(lik.mk2, q10 ~ q01) # q values constrained to be equal
-
-# run ML analysis using intial guess of 0.1 again
-fit.mk1 <- find.mle(lik.mk1, .1, method="subplex")
-anova(fit.mk2, mk1=fit.mk1)
-#      Df   lnLik    AIC    ChiSq   Pr(>|Chi|)   
-# full  2  -26.522  57.044                     
-# mk1   1  -31.095  64.191  9.1466   0.002492
-
-# we can conclude that the asymmetric Markov model fits better; the rate of evolution from state 1 to state 0 is greater than that of state 0 to state 1
-
-# visualize the tree (watch out, it's big!) with reconstructed character states at all nodes
-plot(tree, show.tip.label=FALSE, no.margin=TRUE)
-col <- c("#004165", "#eaab00")
-tiplabels(col=col[states+1], pch=19, adj=1) # use state, but add 1 to give 1s and 2s instead of 0s and 1s
-nodelabels(col=col[attr(states, "node.state")+1], pch=19)
-
-########################################################
-### effects of dropping data from a tree
-########################################################
-
-# how sensitive are parameters to varying amounts of missing data
-# how does tree size affect this
-# what about when:
-#        q01=q10
-#        q01>q10
-
-###########################
-### For trees with q01=q10
-###########################
-
-# simulate a tree as before, with equal rate of character evolution in both directions
-set.seed(2)
-tree <- tree.bd(c(.2, .03), max.taxa=2000)
-set.seed(1)
-states <- sim.character(tree, pars=c(.1, .1), model="mk2") #q01=0.1, q10=0.1
-length(which(states==0))
-
-# build likelihood model and run ML analysis with initial guess
-lik <- make.mk2(tree, states)
-fit <- find.mle(lik, x.init=c(.1, .1))
-
-# MLEs for character evolution rates
 coef(fit)
 
-# store MLEs
+# store MLE of parameter
 r01 <- fit$par[1] # MLE of q01
-r10 <- fit$par[2] # MLE of q10
 
-# build trees to represent 10, 20,..., 1980 tips missing from the original tree
-# first, make a sequence to iterate the loop across various quantities of missing data
-s <- seq(10, 1990, by=10)
+# repeat for many trees varying in amount of missing data
+# build trees to represent n, 2n,...max.taxa tips missing from the original tree
+# first, make a sequence to iterate the loop across various quantities of missing data, dropping n.drop tips each iteration
+n.drop <- 10
+
+s <- seq(10, max.taxa-n.drop, by=n.drop)
 length(s)
 
-# use a loop to simulate and analyse many trees, each containing 10 fewer tips than the previous run
-for (i in 1:length(s)){
-  to_drop <- sample(tree$tip.label, size=s[i])
-  new_tree <- drop.tip(tree, tip=to_drop)
-  new_states <- states[new_tree$tip.label]
-  new_lik <- make.mk2(new_tree, new_states)
-  new_fit <- find.mle(new_lik, x.init=c(.1, .1))
-  r01es <- new_fit$par[1]
-  r10es <- new_fit$par[2]
-  r01 <- c(r01, r01es)
-  r10 <- c(r10, r10es)
-}
+# to drop data randomly, use sample(x, size, prob=NULL)
+prob <- NULL
 
-# visualize what happens to our parameter estimates when data is missing
-plot(s, r01)
-plot(s, r10)
+# to drop data with bias towards sampling a given state more than the other, use the following function:
 
-# compare our model to a null model
-lik_diff <- make.mk2(tree, states)
-lik_same <- constrain(lik_diff, q01~q10)
-
-fit_diff <- find.mle(lik_diff, c(.1, .1))
-fir_same <- find.mle(lik_same, .1)
-anova(fit_diff, fit_same)
-
-# do it for trees missing varying amounts of data
-
-
-
-###################
-# If q01<q10
-###################
-# simulate a tree as before, but with lower rate of character evolution from state 0 to 1
-set.seed(2)
-tree <- tree.bd(c(.2, .03), max.taxa=2000)
-set.seed(1)
-states <- sim.character(tree, pars=c(.1, .3), model="mk2")
-length(which(states==0))
-
-# build likelihood model and run ML analysis with initial guess
-lik <- make.mk2(tree, states)
-fit <- find.mle(lik, x.init=c(.1, .1))
-
-# MLEs for character evolution rates
-coef(fit)
-
-# store MLEs
-r01 <- fit$par[1] # MLE of q01
-r10 <- fit$par[2] # MLE of q10
-
-# build trees to represent 10, 20,..., 1980 tips missing from the original tree
-# first, make a sequence to iterate the loop across various quantities of missing data
-s <- seq(10, 1990, by=10)
-length(s)
-
-# use a loop to simulate and analyse many trees, each containing 10 fewer tips than the previous run
-for (i in 1:length(s)){
-  to_drop <- sample(tree$tip.label, size=s[i])
-  new_tree <- drop.tip(tree, tip=to_drop)
-  new_states <- states[new_tree$tip.label]
-  new_lik <- make.mk2(new_tree, new_states)
-  new_fit <- find.mle(new_lik, x.init=c(.1, .1))
-  r01es <- new_fit$par[1]
-  r10es <- new_fit$par[2]
-  r01 <- c(r01, r01es)
-  r10 <- c(r10, r10es)
-}
-
-length(r01)
-
-plot(c(s, 2000), r10)
-
-### effects of biased sampling data from a tree
-# bias in the probability of a tip being dropped due to its character state
-# how sensitive are parameters to varying amounts of missing biased data
-# how does tree size affect this
-
-# a function that takes in list of character states, a state on which to focus the bias, and a probability of being dropped, and returns a vector of probabilities to pass to sample()
+# function that takes in list of character states, a state on which to focus the bias, and a probability of being dropped, and returns a vector of probabilities to pass to sample() prob parameter
 # e.g. for bias(states, 0, 0.6), Pr[dropping taxa with state=0] = 0.6
 bias <- function(d, s=1, p=.5){
   return(as.numeric(ifelse(d==s, p, 1-p)))
 }
 
-set.seed(2)
-tree <- tree.bd(c(.2, .03), max.taxa=2000)
-set.seed(1)
-states <- sim.character(tree, c(.1, .2), model="mk2")
+# prob <- bias(states, 1, 0.2)
 
-# taxa in state 1 have P=0.2 of being missed in a sample (dropped from the tree)
-p.drop <- bias(states, 1, 0.2)
-
+# use a loop to simulate and analyse many trees, each containing n fewer tips than the previous run
+# run ML analysis for each tree, and keep track of the parameter estimate
 for (i in 1:length(s)){
-  to_drop <- sample(tree$tip.label, size=s[i], prob=p.drop)
+  to_drop <- sample(tree$tip.label, size=s[i], prob=prob)
   new_tree <- drop.tip(tree, tip=to_drop)
   new_states <- states[new_tree$tip.label]
   new_lik <- make.mk2(new_tree, new_states)
   new_fit <- find.mle(new_lik, x.init=c(.1, .1))
   r01es <- new_fit$par[1]
-  r10es <- new_fit$par[2]
   r01 <- c(r01, r01es)
-  r10 <- c(r10, r10es)
 }
 
-########
-set.seed(2)
-tree <- tree.bd(c(.2, .03), max.taxa=40)
-set.seed(1)
-states <- sim.character(tree, pars=c(.1, .1), model="mk2")
-length(which(states==1))
+# visualize what happens to our parameter estimates when data is missing
+plot(c(0,s), r01)
 
-to_drop <- sample(tree$tip.label, size=20)
-new_tree <- drop.tip(tree, tip=to_drop)
-new_states <- states[new_tree$tip.label]
-new_states
+# compare our Markov model fit to a null hypothesis model (with parameters q01 = q10) to test if the Mk fit is statistically justified
+# H: the rates are different
+lik_diff <- make.mk2(tree, states)
 
-p.drop <- bias(states, 1, 0.5)
-p.drop
+# build likelihood function based on null model of q01 = q10
+# Ho: there is no difference in the two rates
+lik_same <- constrain(lik_diff, q01~q10)
+
+# run ML analysis using an initial parameter guess of (0.1, 0.1)
+fit_diff <- find.mle(lik_diff, c(.1, .1))
+
+# run ML analysis using initial guess of 0.1 again
+fit_same <- find.mle(lik_same, .1)
+anova(fit_diff, fit_same)
+# extract the p-value
+anova(fit_diff, fit_same)[2,5]
+
+# if p < 0.05, we can conclude that the asymmetric Markov model fits better; the rates are not equal
+
+# conduct the same hypothesis test for many trees varying in amount of missing data
+# in each iteration, we compare our Markov model fit to a null hypothesis model (with parameters q01 = q10) to test if the Mk fit is statistically justified
+# to do this, we need to accumulate a vector of the anova results. Start with an empty list
+result <- numeric()
+
+for (i in 1:length(s)){
+  to_drop <- sample(tree$tip.label, size=s[i], prob=prob)
+  new_tree <- drop.tip(tree, tip=to_drop)
+  new_states <- states[new_tree$tip.label]
+  new_lik_diff <- make.mk2(new_tree, new_states)
+  new_lik_same <- constrain(new_lik_diff, q01~q10)
+  fit_diff <- find.mle(new_lik_diff, c(.1, .1))
+  fit_same <- find.mle(new_lik_same, .1)
+  sig <- anova(fit_diff, fit_same)
+  if (sig[2,5] <= 0.05){
+    res <- 0 # reject null hypothesis
+  } else {
+    res <- 1 # fail to reject null hypothesis
+  }
+  result <- c(result, res)
+}
+
+plot(s, result)
+
+# we know the true model parameters in advance, so we can measure the effect of missing data on the ability of a hypothesis test to reject a false null hypothesis
+# if we know the null hypothesis is true, we can measure Type 1 error
+
+# repeat analysis for a smaller tree (change max.taxa), for different q01, lambda and mu 
 
 
-to_drop <- sample(tree$tip.label, size=20, prob=p.drop)
-new_tree <- drop.tip(tree, tip=to_drop)
-new_states <- states[new_tree$tip.label]
-new_states
 
+# if interested, we can plot with labeled node states
 plot(tree, show.tip.label=FALSE, no.margin=TRUE)
 col <- c("#004165", "#eaab00")
 tiplabels(col=col[states+1], pch=19, adj=1) # use state, but add 1 to give 1s and 2s instead of 0s and 1s
