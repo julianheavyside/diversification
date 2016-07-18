1dd0aa33a4194981fa3c8a044391990fa36c41f0
Julian Heavyside <julianheavyside@gmail.com>
2016-07-18 13:05:19 -0700

    added functions from Matt's example code in issue #2

--- supporting-functions.R
+++ supporting-functions.R
@@ -20,4 +20,60 @@ standard.error <- function(x){
   sqrt(var(x)/length(x))
 }
 
+## Simulate i datasets, drawing parameters from a distribution
+
+simulate_mk2 <- function(n, r=c(0.1,0.1)){
+  t <- tree.bd(c(1,0), max.taxa=n)
+  d <- sim.character(t,r, model="mk2")
+  if (length(unique(d)) == 1){
+    return(NULL)
+  } else {
+    return(list(t=t, d=d, r=r))
+  }
+}
+
+fit_mk2 <- function(x){
+  l <- make.mk2(x$t,x$d)
+  f <- find.mle(l, x.init=x$r)
+  out <- c(Ntip(x$t), x$r[1], x$r[2],
+           f$par["q01"], f$par["q10"])
+  names(out) <- c("n", "sim_q01","sim_q10", "est_q01", "est_q10")
+  out
+}
+
+sample_phydat <- function(x, s){
+  n     <- Ntip(x$t)
+  drop  <- sample(x$t$tip.label, round((1-s)*n))
+  new_t <- drop.tip(x$t, tip=drop)
+  new_d <- x$d[new_t$tip.label]
+  list(t=new_t, d=new_d, r=x$r)
+}
+
+## Using rejection algorithm to get rid of failures
+## Subsample without bias
+simulate_mk2_rsamp <- function(i, n, s, r=c(0.1,0.1)){
+  out <- data.frame()
+  while(1){
+    dat  <- simulate_mk2(n, r)
+    if (!is.null(dat)) {
+      sam  <- sample_phydat(dat, s)
+      if (length(unique(sam$d)) !=  1) {
+        fit  <- fit_mk2(dat)
+        sfit <- fit_mk2(sam)
+        ## Collect and clean output
+        res <- c(fit, s, sfit)
+        res <- unique(res)
+        out <- rbind(out, res)
+      }
+    }
+    if (nrow(out) == i)
+      break()
+  }
+  
+  names(out) <- c("n", "sim_q01","sim_q10", "est_q01", "est_q10", "samp_f", "n_samp", "est_q01_samp", "est_q10_samp")
+  out
+}
+
+
+
 
