aab99849309eb4dffc1b0b0a3b222b7f6274e33f
Julian Heavyside <julianheavyside@gmail.com>
2016-08-22 14:38:33 -0700

    the function def

--- R/plotting-functions.R
+++ R/plotting-functions.R
@@ -1,2 +1,14 @@
 library(ggplot2)
+library(gridExtra)
+library(grid)
+
+plot_by_filter <- function(dat, q01, n.taxa){
+  dat <- subset(dat, sim_q01 == q01 & n == n.taxa)
+  ggplot(dat, aes(x = samp_f, y = est_q01_samp, colour = bias)) +
+  geom_point() +
+  geom_smooth() +
+  facet_grid(.~n) +
+  geom_hline(yintercept=q01, linetype = "dashed") +
+  labs(x = "Proportion of tips remaining", y = "Parameter estimate")
+  }
 
