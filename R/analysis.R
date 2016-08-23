96c4b1f052f1670a0a83a72f7d715fdfbc44c1c1
Julian Heavyside <julianheavyside@gmail.com>
2016-08-23 16:38:27 -0700

    addded some comments to code

--- R/analysis.R
+++ R/analysis.R
@@ -105,15 +105,18 @@ dat <- res %>%
 
 # plotting ----------------------------------------------------------------
 source("plotting-functions.R")
-fil_plot(400, 0.1)
+# fil_plot(400, 0.1) # a test plot
 
-## make 9 plots: for n = 100, 400, 800, and q01 = 0.1, 0.5, 1
+## make 16 plots: for n = 200, 400, 600, 800, and q01 = 0.1, 0.5, 0.75, 1
 ## make a list of these combinations for subsetting the original dataset
 
-Ns <- c(200, 400, 800)
-Q01s <- c(0.1, 0.5, 1)
+Ns <- c(200, 400, 600, 800)
+Q01s <- c(0.1, 0.5, 0.75, 1)
 
 ## this isn't working
+
+## I want to produce a list of plots, to pass to grid.arrange. having trouble with accumulating a list of plots with loops
+
 # plots <- list()
 # for(i in seq_along(Ns)){
 #   for(j in seq_along(Q01s)){
@@ -122,35 +125,40 @@ Q01s <- c(0.1, 0.5, 1)
 #   }
 # }
 
+## blank space for place holder in first row, last column (top right in grid arrangement)
 blankPlot <- ggplot() + geom_blank(aes(1, 1)) + cowplot::theme_nothing()
 
 ## do it manually for now
-grid.arrange(textGrob("n = 200"),
+grid.arrange(textGrob("n = 200"), # text above the plots, indicating tree size
              textGrob("n = 400"),
+             textGrob("n = 600"),
              textGrob("n = 800"),
              blankPlot,
-             fil_plot(200, 0.1), 
+             fil_plot(200, 0.1), # individual calls to fil_plot function, with n and q01 specified
              fil_plot(400, 0.1),
+             fil_plot(600, 0.1),
              fil_plot(800, 0.1),
-             textGrob("Simulated\ntrees using\nq01=0.1\n(Dashed\nline)", gp = gpar(fontsize = 10)),
+             textGrob("Simulated\ntrees using\nq01=0.1\n(Dashed\nline)", gp = gpar(fontsize = 10)), # text in last column to give info
              fil_plot(200, 0.5),
              fil_plot(400, 0.5),
+             fil_plot(600, 0.5),
              fil_plot(800, 0.5),
              textGrob("q01=0.5", gp = gpar(fontsize = 10)),
              fil_plot(200, 0.75), 
              fil_plot(400, 0.75),
+             fil_plot(600, 0.75),
              fil_plot(800, 0.75),
              textGrob("q01=0.75", gp = gpar(fontsize = 10)),
              fil_plot(200, 1),
              fil_plot(400, 1),
+             fil_plot(600, 1),
              fil_plot(800, 1),
              textGrob("q01=1.0", gp = gpar(fontsize = 10)),
-             ncol = 4,
-             widths = c(rep(12, 3), 5),
-             heights = c(1, rep(12, 4)),
-             bottom="Proportion of tips remaining", 
+             ncol = 5,
+             widths = c(rep(12, 4), 5), # adjustments for column widths
+             heights = c(1, rep(12, 4)), # adjustments for row heights
+             bottom="Proportion of tips remaining after removing data with bias\n(Subsampled with bias (see legend) towards dropping tips in state 1)", 
              left="Estimated parameter (q01)")
-grid.rect(gp=gpar(fill=NA))
 
 # scratch pad -------------------------------------------------------------
 
